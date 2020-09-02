1ca4e125dcbe703edabb698ab4f8800bfc4b3373
UET-Boy <Spellbreaker907@gmail.com>
2020-09-02 09:16:20 +0500

    restarted learning

---
+++ Basic Programs/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "There once was a man named Hamza" << endl;
+    cout << "He was 25 Years old" << endl;
+    cout << "He liked being Hamza" << endl;
+    cout << "But he didn't like being 25" << endl;
+    return 0;
+}
