74130f91afd86202e8aaf7c399137b85c49182d3
UET-Boy <Spellbreaker907@gmail.com>
2022-11-27 16:24:17 +0500

    updated 27nov

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, this is Hamza. "<< endl;
-    cout << "This is my first C++ Program. "<< endl;
+    cout << "Hello, this is Hamza. " << endl;
+    cout << "This is my first C++ Program. " << endl;
+    cout << "Updated date : 27 NOV 2022" << endl;
     cout << "Happy Learning!";
     return 0;
 }
