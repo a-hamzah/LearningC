4e55598d58cd5eefd3774a6f069d3db8ffa5d95b
UET-Boy <Spellbreaker907@gmail.com>
2022-12-04 10:52:10 +0500

    Library PC

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
-int main(){
-    cout << "Hello, this is Hamza. " << endl;
-    cout << "This is my first C++ Program. " << endl;
-    cout << "Updated date : 27 NOV 2022" << endl;
-    cout << "Happy Learning!";
+int main()
+{
+    auto now = chrono::system_clock::now();
+    time_t end_time = chrono::system_clock::to_time_t(now);
+
+    cout << "\nCurrent Time and Date: " << ctime(&end_time) << std::endl;
+
+    cout << "Compiler Paths are OK!, Start Programming...\n"
+         << endl;
+
     return 0;
 }
