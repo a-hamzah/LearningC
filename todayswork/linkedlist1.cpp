98cd7f8299050d0ab17e382c8332795d2fdd08c4
UET-Boy <Spellbreaker907@gmail.com>
2022-12-04 14:27:05 +0500

    linked list full

--- todayswork/linkedlist1.cpp
+++ todayswork/linkedlist1.cpp
@@ -20,7 +20,7 @@ void printList(Node *aHead) // receiving the argument passed by invocation in ma
     }
 }
 
-// this function adds a new node before the head, now, it will become head itself
+//! this function adds a new node before the head, now, it will become head itself
 
 void insertAtTheFront(Node **head, int newValue)
 {
@@ -33,9 +33,10 @@ void insertAtTheFront(Node **head, int newValue)
     *head = newNode;
 }
 
-// this function will an element at the end of the list and mark it as the last node
+//! this function will add an element at the end of the list and mark it as the last node
 
 void insertAtTheEnd(Node **head, int newValue)
+
 {
     // 1. Prepare a new node
     Node *newNode = new Node();
@@ -59,6 +60,23 @@ void insertAtTheEnd(Node **head, int newValue)
     last->next = newNode;
 }
 
+//! this function will add an element in the middle of the linked list
+
+void insertAfter(Node *previous, int newValue)
+{
+    // 1. Check if the previous node is NULL
+    if (previous == NULL)
+    {
+        cout << "Previous cannot be NULL.";
+        return;
+    }
+    // 2. Prepare a newNode
+    Node *newNode = new Node();
+    newNode->value = newValue;
+    newNode->next = previous->next;
+    previous->next = newNode;
+    // 3. Insert a newNode after previous
+}
 int main()
 {
     Node *head = new Node();
@@ -73,13 +91,11 @@ int main()
     third->next = NULL;
 
     insertAtTheFront(&head, -1); // invoking the function and passing arguments
-    insertAtTheFront(&head, -2);
-    insertAtTheFront(&head, 7);
-
-    // invoking the function
 
     insertAtTheEnd(&head, 4);
-    insertAtTheEnd(&head, 5);
+
+    insertAfter(second, 8); // inserting element in the middle of the list after 2
+
     printList(head); // invoking the function and passing argument
 
     return 0;
