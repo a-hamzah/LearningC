254813f068e69485ba839ba8ce8be9b3a5b05f2a
UET-Boy <Spellbreaker907@gmail.com>
2022-12-05 13:27:06 +0500

    changed hello

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Hello, this is Hamza. " << endl;
     cout << "This is my first C++ Program. " << endl;
     cout << "Updated date : 27 NOV 2022" << endl;
-    cout << "Happy Learning!";
+    cout << "Star Learning!";
     return 0;
 }
